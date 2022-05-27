5b28406629b1b001f787584cf6d89dd64ddda1f7
Zannatul Naim <97250227+Zannatul-Naim@users.noreply.github.com>
2022-05-28 00:32:24 +0600

    (Corrected) Matrix addition using 2D array.

--- Array/Matrix/addition.c
+++ Array/Matrix/addition.c
@@ -2,40 +2,48 @@
 #include<stdlib.h>
 
 int main() {
+    freopen("input.txt", "r", stdin);
+    freopen("output.txt", "w", stdout);
+    int row1, col1, row2, col2;
+    printf("Enter Row number of first matrix: ");
+    scanf("%d", &row1);
     
-    int row, col;
-    printf("Enter Row number: ");
-    scanf("%d", &row);
-    printf("Enter Column number: ");
-    scanf("%d", &col);
+    printf("Enter Column number of first matrix: ");
+    scanf("%d", &col1);
 
-    if(row == col) {
-        int matrix_1[row][col];
+    printf("Enter Row number of second matrix: ");
+    scanf("%d", &row2);
+
+    printf("Enter Column number of second matrix: ");
+    scanf("%d", &col2);
+
+    if(row1 == row2  && col1 == col2) {
+        int matrix_1[row1][col1];
     printf("Enter elements of Matrix 1: \n");
-    for(int i = 0; i < row; i++) {
-        for(int j = 0; j < col; j++) {
+    for(int i = 0; i < row1; i++) {
+        for(int j = 0; j < col1; j++) {
             scanf("%d", &matrix_1[i][j]);
         }
     }
 
-    int matrix_2[row][col];
+    int matrix_2[row2][col2];
     printf("Enter elements of Matrix 2: \n");
-    for(int i = 0; i < row; i++) {
-        for(int j = 0; j < col; j++) {
+    for(int i = 0; i < row2; i++) {
+        for(int j = 0; j < col2; j++) {
             scanf("%d", &matrix_2[i][j]);
         }
     }
 
-    int matrix_sum[row][col];
+    int matrix_sum[row1][col1];
     
-    for(int i = 0; i < row; i++) {
-        for(int j = 0; j < col; j++) {
+    for(int i = 0; i < row1; i++) {
+        for(int j = 0; j < col1; j++) {
             matrix_sum[i][j] = matrix_1[i][j] + matrix_2[i][j];
         }
     }
 
-    for(int i = 0; i < row; i++) {
-        for(int j = 0; j < col; j++) {
+    for(int i = 0; i < row1; i++) {
+        for(int j = 0; j < col1; j++) {
             printf("%d ", matrix_sum[i][j]);
         }
         printf("\n");
