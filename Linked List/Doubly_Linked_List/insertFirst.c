cce73347c8328922e0743e87bdd17f205ce77c76
Zannatul Naim <97250227+Zannatul-Naim@users.noreply.github.com>
2022-06-13 21:47:46 +0600

    Insert first positio to a doubly Linked List.

---
+++ Linked List/Doubly_Linked_List/insertFirst.c
@@ -0,0 +1,55 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+struct Node {
+    int data;
+    struct Node* next;
+    struct Node* prev;
+} *head, *tail;
+
+typedef struct Node node;
+
+void insertFirst(int data) {
+    node *newNode = (node*) (malloc(sizeof(node)));
+
+    newNode -> data = data;
+    newNode -> prev = NULL;
+    newNode -> next = head;
+
+    if(head != NULL) head -> prev = newNode;
+    head = newNode;
+
+}
+
+void display() {
+    node * temp;
+
+    temp = head;
+
+    printf("Printing forward...\n");
+    while(temp != NULL) {
+        printf("%d ", temp->data);
+        tail = temp;
+        temp = temp->next;
+    }
+    printf("\n");
+
+    printf("Printing backward...\n");
+    temp = tail;
+    while(temp != NULL) {
+        printf("%d ", temp->data);
+        temp = temp->prev;
+    }
+    printf("\n");
+}
+
+int main() {
+
+
+    for(int i = 1; i <= 10; i++) insertFirst(i);
+
+    display();
+
+    return 0;
+}
