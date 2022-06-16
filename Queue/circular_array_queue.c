545a63e5ff5fbd29e1e4f19bde18a7c367a6adf0
Zannatul Naim <97250227+Zannatul-Naim@users.noreply.github.com>
2022-06-16 08:58:08 +0600

    Circular Queue using Array (fixed)

--- Queue/circular_array_queue.c
+++ Queue/circular_array_queue.c
@@ -27,10 +27,13 @@ void enqueue(int item) {
 
 void dequeue() {
     if(isEmpty()) printf("Nothing to dequeue...\n");
+    else if(front == rear) {
+      front = rear = -1;
+    }
     else {
         int temp = front;
         printf("Dequeued Element is : %d\n", queue[temp]);
-        ++front;
+        front = (front+1)%size;
     }
 }
 
@@ -58,16 +61,37 @@ int main() {
     enqueue(5);
     enqueue(4);
     enqueue(3);
-    enqueue(2);
-    enqueue(1);
+    
     display();
+    
     dequeue();
+    
     display();
+    
     enqueue(6);
+    
     dequeue();
+    dequeue();
+    
     enqueue(7);
+    
     display();
+    
     peek();
+    
+    dequeue();
+    dequeue();
+    
+    display();
+    
+    dequeue();
+    
+    enqueue(100);
+    enqueue(1000);
+    
+    dequeue();
+    
+    display();
 
     return 0;
 }
@@ -76,12 +100,17 @@ int main() {
 
 Output:
 
-Queue is : 5 4 3 2 1
+Queue is : 5 4 3
 Dequeued Element is : 5
-Queue is : 4 3 2 1
+Queue is : 4 3
 Dequeued Element is : 4
-Queue is : 3 2 1 6 7
-Front value of the queue : 3
+Dequeued Element is : 3
+Queue is : 6 7
+Front value of the queue : 6
+Dequeued Element is : 6
+Nothing in the queue
+Nothing to dequeue...
+Dequeued Element is : 100
+Queue is : 1000
 
 */
-
