e26eb62b866651bc8407051f144fda0cb2d6b4aa
Zannatul Naim <zannatulnaim09@gmail.com>
2022-07-05 06:36:57 +0600

    Binary

---
+++ Searching/binary_search.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int binarySearch(int arr[], int l, int r, int item)
+{
+    while (l <= r) {
+        int mid = l + (r - l) / 2;
+  
+        // if item is at mid
+        if (arr[mid] == item)
+            return mid;
+  
+        if (arr[mid] < item)
+            l = mid + 1;
+  
+        else
+            r = mid - 1;
+    }
+  
+    return -1;
+}
+  
+int main(void)
+{
+    // array needs to be sorted to impliment binary search
+    int arr[] = {10, 20, 30, 40, 50, 60, 70, 80};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int item = 30;
+    
+    int position = binarySearch(arr, 0, n - 1, item);
+    
+    if(position == -1)
+        printf("%d Not Found",item);
+    else
+        printf("%d Found at index : %d",item, position);
+        
+    return 0;
+}
