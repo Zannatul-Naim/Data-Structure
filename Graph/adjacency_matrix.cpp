d9e64fc7a98cce933168a7aecb8e7c7bc5c80821
Zannatul Naim <zannatulnaim09@gmail.com>
2022-08-03 19:01:08 +0600

    Simple graph representation using adjacency matrix

---
+++ Graph/adjacency_matrix.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+
+using namespace std;
+
+int main() {
+    
+    int vertex;
+    cout << "Enter number of vertex : ";
+    cin >> vertex;
+
+    int edges;
+    cout << "Enter Number of edges : ";
+    cin >> edges;
+
+    int adjacencyMatrix[vertex][vertex];
+    for(int i = 0; i < vertex; i++) {
+        for(int j = 0; j < vertex; j++) {
+            adjacencyMatrix[i][j] = 0;
+        }
+    }
+
+    cout << "Enter edges : \n";
+    for(int i = 0; i < edges; i++) {
+        int u, v;
+        cin >> u >> v;
+        if(u > vertex || v > vertex) {
+            cout << "Invalid edge!";
+            i--;
+        }
+        else {
+        adjacencyMatrix[u][v] = 1;
+        adjacencyMatrix[v][u] = 1;
+        }
+    }
+
+    // printing adjacency matrix...
+    for(int i = 0; i < vertex; i++) {
+        for(int j = 0; j < vertex; j++) {
+            cout << adjacencyMatrix[i][j] << " ";
+        }
+        cout << "\n";
+    }
+
+    return 0;
+}
