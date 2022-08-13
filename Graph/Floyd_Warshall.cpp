c041a55dc1b0df871bb5da7548babcf236f74894
Zannatul Naim <zannatulnaim09@gmail.com>
2022-08-13 13:14:17 +0600

    Strongly Connected part (Updated)

--- Graph/Floyd_Warshall.cpp
+++ Graph/Floyd_Warshall.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 int adjMatrix[20][20];
 int powMatrix[4][20][20];
+int strongMatrix[20][20];
 
 /*
 0 0 0 1
@@ -41,6 +42,19 @@ void createPowMatrix(int n) {
             }
         }
     }
+}
+
+bool is_strongly_connected(int n) {
+    int cnt = 0;
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            strongMatrix[i][j] = 0;
+            strongMatrix[i][j] = powMatrix[0][i][j] + powMatrix[1][i][j] + powMatrix[2][i][j] + powMatrix[3][i][j];
+            if(strongMatrix[i][j] == 0) cnt++; 
+        }
+    }
+    if(cnt > 0) return true;
+    else return false;
 }
 
 void printMatrix(int n) {
@@ -83,5 +97,9 @@ int main() {
 
     detailedPrint(n);
 
+    cout << endl << endl;
+    if(is_strongly_connected(n)) cout << "Strongly Connected!" << endl;
+    else cout << "Not Strongly Connected!" << endl;
+    
     return 0;
 }
